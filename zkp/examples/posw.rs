0406fe7d53735297e179d72c8a4eb7ec24c8aa15
sherry <adamgarysdevil@gmail.com>
2022-07-30 00:50:11 +0800

    -

--- zkp/examples/posw.rs
+++ zkp/examples/posw.rs
@@ -8,6 +8,8 @@ use rand_chacha::ChaChaRng;
 
 fn main() {
     let rng = &mut ChaChaRng::seed_from_u64(1234567);
+    // let difficulty_target: u64 = 18446744073709551615; // block.difficulty_target()
+    let difficulty_target: u64 = 1844674407370955161;
 
     // Construct the block template.
     let block = Testnet2::genesis_block();
@@ -15,7 +17,7 @@ fn main() {
         block.previous_block_hash(),
         block.height(),
         block.timestamp(),
-        block.difficulty_target(),
+        difficulty_target,
         block.cumulative_weight(),
         block.previous_ledger_root(),
         block.transactions().clone(),
@@ -27,20 +29,21 @@ fn main() {
             .unwrap(),
     );
 
+    println!("Difficulty_target is: {:?}", difficulty_target);
     for i in 0..10 {
         let start = Instant::now();
         Testnet2::posw()
             .mine(&block_template, &AtomicBool::new(false), rng)
             .unwrap();
         let duration = start.elapsed();
-        println!("{}. Time elapsed in generating a valid proof() is: {:?}", i, duration);
+        println!(
+            "{}. Time elapsed in generating a valid proof() is: {:?}",
+            i, duration
+        );
     }
 
     // let _is_valid = Testnet2::posw().verify_from_block_header(Testnet2::genesis_block().header());
 }
-
-
-
 
 // println!("生成一个符合难度的证明");
 // println!("验证一个证明");
