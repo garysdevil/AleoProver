859ea42758687c2a449e03e48b61269751853403
sherry <adamgarysdevil@gmail.com>
2022-08-29 23:45:42 +0800

    fix

--- zkp-testnet2-gpu/examples/posw_rayon_tokio_gpu.rs
+++ zkp-testnet2-gpu/examples/posw_rayon_tokio_gpu.rs
@@ -25,12 +25,15 @@ fn get_thread_pools() -> Vec<Arc<ThreadPool>> {
     return get_thread_pools_gpu();
 }
 
+const CUDA_NUMS: i16 = 3;
+const CUDA_JOBS: i16 = 6;
+
 fn get_thread_pools_gpu() -> Vec<Arc<ThreadPool>> {
     let mut thread_pools: Vec<Arc<ThreadPool>> = Vec::new();
 
-    let cuda_num = 3;
-    let cuda_jobs = 6;
-    let total_jobs = cuda_jobs * cuda_num;
+    // let CUDA_NUMS = 3;
+    // let CUDA_JOBS = 6;
+    let total_jobs = CUDA_JOBS * CUDA_NUMS;
     for index in 0..total_jobs {
         let pool = ThreadPoolBuilder::new()
             .stack_size(8 * 1024 * 1024)
@@ -40,22 +43,24 @@ fn get_thread_pools_gpu() -> Vec<Arc<ThreadPool>> {
             .unwrap();
         thread_pools.push(Arc::new(pool));
     }
-    println!("Pools  cuda_num={}, cuda_jobs={}", cuda_num, cuda_jobs);
+    println!("Pools  CUDA_NUMS={}, CUDA_JOBS={}", CUDA_NUMS, CUDA_JOBS);
     thread_pools
 }
 
 async fn mine(thread_pools: Vec<Arc<ThreadPool>>) {
     let mut joins = Vec::new();
     let block_template = posw::get_genesis_template();
+    let mut total_jobs = CUDA_JOBS * CUDA_NUMS;
     for tp in thread_pools.iter() {
         let tp = tp.clone();
         let block_template = block_template.clone();
-        // joins.push(task::spawn(async move {
+        total_jobs -= 1;
+        let gpu_index = total_jobs%CUDA_NUMS;
         joins.push(task::spawn_blocking(move || {
-            // task::spawn_blocking 比 task::spawn 快0.0427s
             tp.install(|| {
                 let start = Instant::now();
-                posw::get_proof_gpu(block_template, rand::random::<u64>(), 0);
+                posw::get_proof_gpu(block_template, rand::random::<u64>(), gpu_index);
+                dbg!(total_jobs);
                 let duration = start.elapsed();
                 println!(
                     "{}. Time elapsed in generating a valid proof() is: {:?}",
