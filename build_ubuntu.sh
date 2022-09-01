00a0ecf761e583e414914300555f67a5608434d8
gary247 <adamgarysdevil@gmail.com>
2022-09-01 17:24:15 +0800

    fix

---
+++ build_ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Aborting: run as root user!"
+    exit 1
+fi
+
+echo "================================================"
+echo " Attention - Building AleoProver from source code."
+echo "================================================"
+
+# Install Ubuntu dependencies
+
+apt-get update
+apt-get install -y \
+    build-essential \
+    curl \
+    clang \
+    gcc \
+    libssl-dev \
+    llvm \
+    make \
+    pkg-config \
+    tmux \
+    xz-utils
+
+# Install Rust
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+# Install snarkOS
+# cargo clean
+cargo install --path .
