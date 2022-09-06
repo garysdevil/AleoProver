08ac20c439dd9cbdcdebcb7bdc5084ff0678385d
gary247 <adamgarysdevil@gmail.com>
2022-09-06 16:50:32 +0800

    -

--- testnet3-storage/src/main.rs
+++ testnet3-storage/src/main.rs
@@ -21,47 +21,42 @@ use snarkvm::console::network::{Network, Testnet3};
 //     network::Testnet3,
 // };
 
-type Testnet3BlockHash = <Testnet3 as Network>::BlockHash;
 
 pub fn main() {
     logger::initialize_logger(2);
 
-    // let path = "/root/vscode/myrust/mylocal/local_path_for_rocksdb_storage";
-    // let data_id = DataID::BlockIDMap;
+    main1();
+    
+}
 
-    // let database = rocksdb::RocksDB::open(Testnet3::ID).unwrap();
-    // let storage = rocksdb::RocksDB::open_map(N::ID, DataID::BlockIDMap).unwrap();
+// fn main2(){
+    
+//     // type Testnet3BlockHash = <Testnet3 as Network>::BlockHash;
+//     // let id_map: DataMap<u32, Testnet3BlockHash>;
+//     let id_map = rocksdb::RocksDB::open_map(Testnet3::ID, DataID::BlockIDMap).unwrap();
+//     let mut key_iter = id_map.keys();
+//     while let Some(element) = &key_iter.next() {
+//         // println!("{}", element);
+//         let value = id_map.get(element).unwrap().unwrap();
+//         println!("{:?}={:?}", element, value);
+//     }
+// }
 
+fn main1(){
+    type Testnet3BlockHash = <Testnet3 as Network>::BlockHash;
+    
     let id_map: DataMap<u32, Testnet3BlockHash>;
     id_map = rocksdb::RocksDB::open_map(Testnet3::ID, DataID::BlockIDMap).unwrap();
+    let key_iter = id_map.keys();
+    let height_max = key_iter.max_by_key(|x| **x).unwrap();
+    let height_max = *height_max;
+    let hash = id_map.get(&height_max).unwrap().unwrap();
+    println!("{}={}", height_max, hash);
 
-    let mut key_iter =
-        <DataMap<u32, Testnet3BlockHash> as MapRead<u32, Testnet3BlockHash>>::keys(&id_map);
-    // let mut value_iter =
-    //     <DataMap<u32, Testnet3BlockHash> as MapRead<u32, Testnet3BlockHash>>::values(&id_map);
-
-
-    // loop {
-    //     match &key_iter.next() {
-    //         None => break,
-    //         Some(element) => {
-    //             // println!("{:?}", element);
-    //             let value = id_map.get(element).unwrap().unwrap();
-    //             println!("{:?}={:?}", element, value);
-    //         }
-    //     }
-    // }
-    while let Some(element) = &key_iter.next() {
-        // println!("{}", element);
-        let value = id_map.get(element).unwrap().unwrap();
-        println!("{:?}={:?}", element, value);
+    let mut value_iter = id_map.values();
+    while let Some(element) = &value_iter.next() {
+        println!("{}", element);
+        // let value = id_map.get(element).unwrap().unwrap();
+        // println!("{:?}={:?}", element, value);
     }
-    // loop {
-    //     match &key_iter.next() {
-    //         None => break,
-    //         Some(element) => {
-    //             println!("{:?}", element);
-    //         }
-    //     }
-    // }
 }
